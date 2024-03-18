dfdd63dd604bfcbe027a032c01acd6066f2545d8
kitubas <henrique_leite1@yahoo.com.br>
2024-03-18 15:09:19 -0300

    testes unitários - final

--- src/templates/Home/Home.test.js
+++ src/templates/Home/Home.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Home } from './index hooks teste';
 
 test('renders learn react link', () => {
